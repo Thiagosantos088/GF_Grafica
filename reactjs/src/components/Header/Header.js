ea204edbcc0e175e0bff294281f8757c24914f3f
Thiagosantos088 <thiago.gr37@gmail.com>
2024-03-08 11:47:48 -0300

    Quarto commit

--- reactjs/src/components/Header/Header.js
+++ reactjs/src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import C from '../../image/logoatu.png'
+import logo from '../../image/logoatu.png'
+import usuario from '../../image/icons8-usuário-72.png'
 import './Header.css';
 import {Link} from 'react-router-dom';
 
@@ -6,12 +7,13 @@ function Header () {
 return(
     <>
     <header>
-        <div className='menu'>
-        <img src={C} alt='' />
+        <div  className='menu'>
+        <img id='logo' src={logo} alt='' />
         <Link to="home">Home</Link>
         <Link to="produtos">Produtos</Link>
         <Link to="produtos">Sobre nos</Link>
         <Link to="contatos">Contatos</Link>
+        <img cl src={usuario} alt='' />
         </div>
         <div className='busca'>
         <input className='botao' type="text" name="busca" id="busca" placeholder='     Procure seu produto' />
